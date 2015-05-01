d31a074eebb8d40d3ca96e47539df76a64ba8538
1101137147 <john830426000@yahoo.com.tw>
2015-05-01 17:12:15 +0800

    1.0

---
+++ MvcApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using KuasCore.Models;
+using KuasCore.Services;
+using KuasCore.Services.Impl;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Http;
+
+namespace MvcApplication1.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+
+        public IEmployeeService EmployeeService { get; set; }
+        
+        [HttpGet]
+        public IList<Employee> GetAllEmployees()
+        {
+            return EmployeeService.GetAllEmployees();
+        }
+        
+        [HttpGet]
+        public Employee GetEmployeeById(string id)
+        {
+            return EmployeeService.GetEmployeeById(id);
+        }
+
+        [HttpGet]
+        public Employee GetEmployeeByName(string name)
+        {
+            return EmployeeService.GetEmployeeByName(name);
+        }
+
+
+    }
+
+}
